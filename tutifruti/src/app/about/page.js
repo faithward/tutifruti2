9f2cf5eef0d2e9612c1639c5fe7f92dd4fe95fcf
Faith <faithward24@gmail.com>
2024-01-22 23:42:19 -0500

    redid team page and what we do page

--- tutifruti/src/app/about/page.js
+++ tutifruti/src/app/about/page.js
@@ -8,24 +8,31 @@ export default function About() {
         <div className="bg-white h-full">
             <Navbar />
             <h1 className="text-center text-4xl font-tutifruti">Meet Our Team</h1>
-            <div className="container bg-lightblue rounded-md w-5/6 my-4 mx-auto">
-                <div className="flex flex-col gap-4 p-6">
-                    <div className="mx-auto">
-                        <Image className="rounded-md object-cover" width={800} height={600} priority src="/gallery/isaromina2.png" alt="The photo is shy but I swear they look cool" />
+            <div className="container bg-blueSquare bg-no-repeat bg-cover mb-2 w-5/6 min-h-screen mx-auto">
+                <div className='flex p-11 gap-3'>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg h-full w-5/6">
+                        <h1 className="text-center text-xl mt-2 font-tutifruti">ISA OSTOS</h1>
+                        <h4 className="text-center pb-3 text-lg font-tutifruti">President</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">Hi! I am Isa and I am a Colombian artist based in South Florida.  I have a Bachelors degree in Anthropology and International Affairs from Florida State University, and an MFA in Documentary Filmmaking from Northwestern University.</p>
+                        <p className="text-center text-sm pb-2 px-4 font-mono">As the President of tutifruti, my vision is to maintain safe spaces for BIPOC, women/girls, and LGBTQ+ skaters where internal growth and physical wellness are encouraged and nurtured.</p>
                     </div>
-                    <div className="flex sm:flex-row flex-col gap-4">
-                        <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-1/2">
-                            <h1 className="text-center text-2xl mt-2 font-tutifruti">ISA OSTOS</h1>
-                            <h4 className="text-center pb-3 text-xl font-tutifruti">President</h4>
-                            <p className="text-center pb-1 px-4 font-mono">Hi! I am Isa and I am a Colombian artist based in South Florida.  I have a Bachelors degree in Anthropology and International Affairs from Florida State University, and an MFA in Documentary Filmmaking from Northwestern University.</p>
-                            <p className="text-center pb-2 px-4 font-mono">As the President of tutifruti, my vision is to maintain safe spaces for BIPOC, women/girls, and LGBTQ+ skaters where internal growth and physical wellness are encouraged and nurtured.</p>
-                        </div>
-                        <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-1/2">
-                            <h1 className="text-center text-2xl mt-2 font-tutifruti">ROMINA PALMERO</h1>
-                            <h4 className="text-center pb-3 text-xl font-tutifruti">Vice President</h4>
-                            <p className="text-center pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
-                            <p className="text-center pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
-                        </div>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-xl mt-2 font-tutifruti">ROMINA PALMERO</h1>
+                        <h4 className="text-center pb-3 text-lg font-tutifruti">Vice President</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
+                        <p className="text-center text-sm pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
+                    </div>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-xl mt-2 font-tutifruti">ROZY</h1>
+                        <h4 className="text-center pb-3 text-lg font-tutifruti">Community Outreach Volunteer</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
+                        <p className="text-center text-sm  pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
+                    </div>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-xl mt-2 font-tutifruti">THANIA</h1>
+                        <h4 className="text-center pb-3 text-lg font-tutifruti">Board Member</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
+                        <p className="text-center text-sm pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
                     </div>
                 </div>
             </div>
@@ -33,3 +40,32 @@ export default function About() {
         </div>
     )
 };
+
+/**
+ *                 <div className='grid grid-rows-1 grid-cols-4 my-2 mx-auto'>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-2xl mt-2 font-tutifruti">ISA OSTOS</h1>
+                        <h4 className="text-center pb-3 text-xl font-tutifruti">President</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">Hi! I am Isa and I am a Colombian artist based in South Florida.  I have a Bachelors degree in Anthropology and International Affairs from Florida State University, and an MFA in Documentary Filmmaking from Northwestern University.</p>
+                        <p className="text-center text-sm pb-2 px-4 font-mono">As the President of tutifruti, my vision is to maintain safe spaces for BIPOC, women/girls, and LGBTQ+ skaters where internal growth and physical wellness are encouraged and nurtured.</p>
+                    </div>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-2xl mt-2 font-tutifruti">ROMINA PALMERO</h1>
+                        <h4 className="text-center pb-3 text-xl font-tutifruti">Vice President</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
+                        <p className="text-center text-sm pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
+                    </div>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-2xl mt-2 font-tutifruti">ROMINA PALMERO</h1>
+                        <h4 className="text-center pb-3 text-xl font-tutifruti">Vice President</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
+                        <p className="text-center text-sm  pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
+                    </div>
+                    <div className="bg-tan border-2 border-solid border-darkblue rounded-lg w-5/6">
+                        <h1 className="text-center text-2xl mt-2 font-tutifruti">ROMINA PALMERO</h1>
+                        <h4 className="text-center pb-3 text-xl font-tutifruti">Vice President</h4>
+                        <p className="text-center text-sm pb-1 px-4 font-mono">I’m Romina, a Venezuelan artist raised in South Florida. I received a BA in Studio Art from Florida State University, with a focus on photography and printmaking. I am passionate about writing, bookmaking, film photography, mental health, and of course, skating!</p>
+                        <p className="text-center text-sm pb-2 px-4 font-mono">As Vice President of tutifruti, I bring my academic experiences and life experiences to my work in order to lead with integrity, compassion, and intentionality.</p>
+                    </div>
+                </div>
+ */
--- tutifruti/src/app/what/page.js
+++ tutifruti/src/app/what/page.js
@@ -27,27 +27,49 @@ export default function What() {
                     </div>
                     <Link className="mx-auto mt-6" target="_blank" href="https://linktr.ee/tutifruti.sk8?lt_utm_source=lt_share_link#302007025"><Image alt="support us" src="supportusbutton.svg" width={120} height={60} /></Link>
                 </div>
-                <div id="photoSection" className="grid grid-rows-4 min-h-screen grid-cols-3 gap-4 m-4">
+                <div id="pair1" className="grid grid-rows-2 grid-cols-3 gap-4 m-4 min-h-screen">
                     <div className="row-span-1 col-span-1 relative block">
                         <Image className="object-cover" src="/gallery/skatergirl.jpg" fill />
                     </div>
                     <div className="row-span-1 col-span-2 relative block">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed lacus urna. Nunc sollicitudin tristique arcu ac mollis. Sed ultricies, mauris id pellentesque imperdiet, ante nunc pretium lacus, ullamcorper volutpat lacus purus non lacus. Quisque et sollicitudin lectus, eget interdum odio. Fusce tortor magna, tristique eu eleifend in, tristique ac est. Vestibulum a velit eu nunc laoreet fermentum. Praesent at velit quam. Maecenas finibus vehicula elit at porttitor. Duis nulla nulla, molestie a ante vel, sollicitudin ultrices ex.</p>
+                        <h1 className='text-2xl underline underline-offset-4 font-tutifruti mb-2'>Private Lessons</h1>
+                        <p className='text-xl'>We offer private individual and group lessons for all ages, all identities, and all levels. Our lessons last an hour and we meet our students at local skateparks– there’s always two coaches on site for additional support. Our coaching is trauma-informed, based on our personal work in mental health, trauma, and our experiences as skaters. As part of our mission, we help students navigate through the emotional blocks that are part of life including those that come up during skating. Private lessons are available every week, 7 days a week. The aim of the private lessons is to offer a structured, safe environment where we focus on specific tricks, lines, and personal goals that our students want to achieve.</p>
                     </div>
                     <div className="row-span-1 col-span-2 relative block">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed lacus urna. Nunc sollicitudin tristique arcu ac mollis. Sed ultricies, mauris id pellentesque imperdiet, ante nunc pretium lacus, ullamcorper volutpat lacus purus non lacus. Quisque et sollicitudin lectus, eget interdum odio. Fusce tortor magna, tristique eu eleifend in, tristique ac est. Vestibulum a velit eu nunc laoreet fermentum. Praesent at velit quam. Maecenas finibus vehicula elit at porttitor. Duis nulla nulla, molestie a ante vel, sollicitudin ultrices ex.</p>
+                    <h1 className='text-2xl underline underline-offset-4 font-tutifruti mb-2'>Skate Clinics</h1>
+                        <p className='text-xl'>We host free or low cost skate clinics (skateboarding and rollerskating) across South Florida. The focus of these clinics is to offer accessible coaching to our local community. The clinics are primarily for beginner level skaters, but we encourage anyone who wants to participate to show up as a participant or volunteer! All clinics are hosted at local skateparks  throughout Miami-Dade, Broward, and Palm Beach counties. Loaner gear is available for participants to borrow, and refreshments (water and snacks) are always part of our programming. Our clinics run for an average of 3 hours which include an hour of active coaching and 2 hours of open skate with coaches present as a resource.</p>
                     </div>
                     <div className="row-span-1 col-span-1 relative block">
                         <Image className="object-cover" src="/gallery/painting2.jpg" fill />
                     </div>
+                </div>
+                <div id="pair2" className="grid grid-rows-2 grid-cols-3 gap-4 m-4 min-h-screen">
                     <div className="row-span-1 col-span-1 relative block">
                         <Image className="object-cover" src="/gallery/class.png" fill />
                     </div>
                     <div className="row-span-1 col-span-2 relative block">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed lacus urna. Nunc sollicitudin tristique arcu ac mollis. Sed ultricies, mauris id pellentesque imperdiet, ante nunc pretium lacus, ullamcorper volutpat lacus purus non lacus. Quisque et sollicitudin lectus, eget interdum odio. Fusce tortor magna, tristique eu eleifend in, tristique ac est. Vestibulum a velit eu nunc laoreet fermentum. Praesent at velit quam. Maecenas finibus vehicula elit at porttitor. Duis nulla nulla, molestie a ante vel, sollicitudin ultrices ex.</p>
+                    <h1 className='text-2xl underline underline-offset-4 font-tutifruti mb-2'>Community Events</h1>
+                        <p className='text-xl'>Community skate events are opportunities for Tutifruti to “take over a skatepark” and give back to the community. These are structured as a social meet up where people show up and skate, make art, and meet new people. Our community events are centered around women, BIPOC, and LGBTQ+ individuals, but all ages and identities are welcome. We often host giveaways at our community events, thanks to our generous partners!</p>
                     </div>
                     <div className="row-span-1 col-span-2 relative block">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed lacus urna. Nunc sollicitudin tristique arcu ac mollis. Sed ultricies, mauris id pellentesque imperdiet, ante nunc pretium lacus, ullamcorper volutpat lacus purus non lacus. Quisque et sollicitudin lectus, eget interdum odio. Fusce tortor magna, tristique eu eleifend in, tristique ac est. Vestibulum a velit eu nunc laoreet fermentum. Praesent at velit quam. Maecenas finibus vehicula elit at porttitor. Duis nulla nulla, molestie a ante vel, sollicitudin ultrices ex.</p>
+                    <h1 className='text-2xl underline underline-offset-4 font-tutifruti mb-2'>Creative Art Workshops</h1>
+                        <p className='text-xl'>One of our main goals in Tutifruti is to encourage diverse coping mechanisms or emotional outlets, and creative art is an incredible way to learn valuable skills in mental health. We conduct low cost, accessible art workshops that teach unique techniques and mediums for all ages. From screenprinting to bookmaking, block printing to painting, we utilize the arts as a vehicle for emotional wellness, creative expression, and self-esteem.</p>
+                    </div>
+                    <div className="row-span-1 col-span-1 relative block">
+                        <Image className="object-cover" src="/gallery/helmet.png" fill />
+                    </div>
+                </div>
+                <div id="pair3" className="grid grid-rows-2 grid-cols-3 gap-4 m-4 min-h-screen">
+                    <div className="row-span-1 col-span-1 relative block">
+                        <Image className="object-cover" src="/gallery/class.png" fill />
+                    </div>
+                    <div className="row-span-1 col-span-2 relative block">
+                    <h1 className='text-2xl underline underline-offset-4 font-tutifruti mb-2'>Mental Health Education</h1>
+                        <p className='text-xl'>We offer peer-led guided discussions related to various mental health topics, in order to maintain safe spaces to talk with like-minded people about complex and impactful circumstances. These discussions may include aspects of mental health education sourced from reputable, evidence-based writing on the topics at hand.</p>
+                    </div>
+                    <div className="row-span-1 col-span-2 relative block">
+                    <h1 className='text-2xl underline underline-offset-4 font-tutifruti mb-2'>Skate Advocacy</h1>
+                        <p className='text-xl'>Part of our work to promote the sport of skating involves municipal advocacy for skateable spaces and skateparks. We attend and speak at commission meetings and town halls whenever possible, to ensure cities and government officials are aware of the positive impacts of skateparks, keep existing ones open, and build more. Decriminalizing and destigmatizing skating is a central tenet of Tutifruti’s work, as it is vital for the sport to be accessible and free of judgment (whether from the law or from individuals) to provide full benefits to a community.</p>
                     </div>
                     <div className="row-span-1 col-span-1 relative block">
                         <Image className="object-cover" src="/gallery/helmet.png" fill />
