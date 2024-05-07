aba2752e3eec1d617af7db6fd19b63f50e08a568
Faith <faithward24@gmail.com>
2024-05-07 06:22:24 -0400

    footer link

--- tutifruti/src/app/components/footer.js
+++ tutifruti/src/app/components/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
     return (
@@ -7,7 +7,7 @@ export const Footer = () => {
             <header className="flex flex-col justify-center text-black font-medium font-tutifruti py-2 bg-lightblue">
                 <p className="mx-auto text-md">tutifruti is a registered 501(c)(3).</p>
                 <p className="mx-auto text-md">FEIN: 92-1511448</p>
-                <p className="mx-auto text-md">website by faith ward.</p>
+                <Link className="mx-auto text-md hover:text-lightred" target="_blank" href="https://www.linkedin.com/in/faithwardtech">website by faith ward.</Link>
             </header>
         </>
     )
